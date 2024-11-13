296bcf31ab27536c74e2262fd6623dddac851b41
pixz <tinycactusking@gmail.com>
2024-11-12 23:17:00 -0500

    Squashed a few bugs, app now works properly so far. Adding tasks and updating their descriptions works now.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='tasks-cli',
-    version='0.0.2',
+    version='0.1.4',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
--- src/__init__.py
+++ src/__init__.py
@@ -15,7 +15,7 @@ class Main:
     # list for tasks
     tasks = []
 
-    # current time string
+    # capture current time in a string
     current_time = datetime.datetime.now().strftime("%m-%d-%Y %H:%M")
 
     # add a task to the task list
@@ -23,7 +23,7 @@ class Main:
         # figure out what the id for the new task will be.
         # we do it this way so that we don't end up with
         # two tasks with the same id after we delete a task
-        new_id = (Main.tasks[Main.tasks.length - 1]["id"]) + 1
+        new_id = (Main.tasks[len(Main.tasks) - 1]["id"]) + 1
 
         # generate new task
         Main.tasks.append({
@@ -34,24 +34,43 @@ class Main:
             "updatedAt": Main.current_time,
         })
 
+        print(f"Added task with id {new_id}")
+
+    # find a task by its id
+    # returns NoneType if none is found
+    def find_by_id(task_id):
+        for x, task in enumerate(Main.tasks):
+            if int(task['id']) == int(task_id):
+                return x
+
     # update task description
-    def update_task(task_id):
-        Main.tasks[task_id]["description"] = Main.args[3]
+    def update_task(pos):
+        # task we are changing
+        current_task = Main.tasks[pos]
+
+        # change the description
+        current_task["description"] = Main.args[3]
+
+        # change the update time
+        current_task["updatedAt"] = Main.current_time
+
+        print(f"Updated  #{current_task['id']
+                           } description to '{Main.args[3]}'")
 
     # delete task
-    def delete_task(task_id):
+    def delete_task(pos):
         pass
 
     # mark a task a todo
-    def mark_task_todo(task_id):
+    def mark_task_todo(pos):
         pass
 
     # mark a task as in progress
-    def mark_task_inprogress(task_id):
+    def mark_task_inprogress(pos):
         pass
 
     # mark a task as done
-    def mark_task_done(task_id):
+    def mark_task_done(pos):
         pass
 
     # list tasks
@@ -65,10 +84,9 @@ class Main:
     # get the task list and load it into a dict
     def load_json():
         with open(Main.path + "tasks-cli.json", "r") as file:
-            Main.tasks = json.loads(file.readline)
+            Main.tasks = json.loads(file.readline())
 
     # store task list dict into json file
-
     def save_json():
         with open(Main.path + "tasks-cli.json", "w") as file:
             file.writelines(json.dumps(Main.tasks))
@@ -88,6 +106,30 @@ class Main:
 
         # Load the json file
         Main.load_json()
+
+        # process args
+        if len(Main.args) > 1:
+            match Main.args[1]:
+                case "add":
+                    if len(Main.args) > 2:
+                        Main.add_task(Main.args[2])
+                    else:
+                        print("Error: Description needed with 'add' argument")
+                case "update":
+                    if len(Main.args) > 2:
+                        if len(Main.args) > 3:
+                            try:
+                                index = Main.find_by_id(Main.args[2])
+                                Main.update_task(index)
+                            except Exception as e:
+                                print(
+                                    f"ERROR: please provide valid integer for id\n {e}")
+                        else:
+                            print("ERROR: please provide valid integer for id")
+                    else:
+                        print("ERROR: please provide the id of the task to update.")
+                case _:
+                    print("ERROR: unknown error occured, likely no flags received")
 
         # save the file
         Main.save_json()
